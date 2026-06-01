d44f97fcb55fd92a8435bd23005ed09a5d77c7ad
Aman Singh <singhaman5467@gmail.com>
2026-06-01 07:07:46 +0545

    Initial Next.js project setup

---
+++ lib/constants.ts
@@ -0,0 +1,8 @@
+export const SITE_CONFIG = {
+  name: "Titan Opticals",
+  description:
+    "A modern eyewear e-commerce website for glasses, sunglasses, lenses, and eye-checkup booking.",
+  phone: "+977-9802776649",
+  email: "watchsunglasses5@gmail.com",
+  address: "Kichapokhari,New Road(Opposite to NMB Bank",
+};
